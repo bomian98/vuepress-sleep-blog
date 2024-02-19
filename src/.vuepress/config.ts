7ff77cc667e98011a0071d2df24286a6ed75d209
wsczh <1998wsc@gmail.com>
2024-02-19 22:58:40 +0800

    first test

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,8 @@
 import { defineUserConfig } from "vuepress";
-import theme from "./theme.js";
+import { getDirname, path } from "vuepress/utils";
+import hopeTheme from "./theme.js";
+
+const __dirname = getDirname(import.meta.url);
 
 export default defineUserConfig({
   base: "/",
@@ -8,7 +11,22 @@ export default defineUserConfig({
   title: "博客演示",
   description: "vuepress-theme-hope 的博客演示",
 
-  theme,
+  theme: hopeTheme(
+    {
+      // 主题选项
+      // ...
+    },
+    { custom: true },
+  ),
+
+  alias: {
+    // 你可以在这里将别名定向到自己的组件
+    // 比如这里我们将主题的主页组件改为用户 .vuepress/components 下的 HomePage.vue
+    "@theme-hope/components/HomePage": path.resolve(
+      __dirname,
+      "./components/HomePage.vue",
+    ),
+  },
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
